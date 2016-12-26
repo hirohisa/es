cbe6ea8d5a43e62c691c780f16633ea68666d839
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2016-12-27 01:08:40 +0900

    Update create/search/delete sample

--- script.rb
+++ script.rb
@@ -26,30 +26,67 @@ class HTTPClient
   def put(request_uri, data, header = {})
     http.put(request_uri, data, header)
   end
+
+  def delete(request_uri, header = {})
+    http.delete(request_uri, header)
+  end
+
 end
 
 class ElasticsearchClient < HTTPClient
 
-  def search(query)
-    get("/_search?q=#{query}*")
+  def search_tag(tag)
+    get("/_search?q=tags:#{tag}")
   end
 
   def create(index, type, id, data)
     put("/#{index}/#{type}/#{id}", data.to_json)
   end
 
+  def remove(index, type, id)
+    delete("/#{index}/#{type}/#{id}")
+  end
+
 end
 
 ## Sample
-client = ElasticsearchClient.new
-
+@client = ElasticsearchClient.new
 index = 'article'
 type = 'image'
 
-# 10.times do |i|
-#   client.create(index, type, i, { data: "a1#{i}b" })
-# end
+def delete_data(index, type, list)
+  list.each do |i|
+    @client.remove(index, type, i)
+  end
+end
 
-result = client.search("a")
+def create_data(index, type)
+  data = { title: "ワンピース", tags: ['action', 'fantasy', 'jump'] }
+  @client.create(index, type, 1, data)
+
+  data = { title: "ナルト", tags: ['action', 'ninja', 'jump'] }
+  @client.create(index, type, 2, data)
+
+  data = { title: "マギ", tags: ['action', 'fantasy', 'sunday'] }
+  @client.create(index, type, 3, data)
+
+  data = { title: "hit", tags: ['game', 'ios'] }
+  @client.create(index, type, 4, data)
+
+  data = { title: "ドラゴンボール", tags: ['toriyama', 'action', 'jump'] }
+  @client.create(index, type, 5, data)
+
+  data = { title: "ドラゴンクエスト", tags: ['game', 'toriyama', 'game'] }
+  @client.create(index, type, 6, data)
+
+  data = { title: "FF", tags: ['game', 'fantasy'] }
+  @client.create(index, type, 7, data)
+end
+
+# delete_data(index, type, [1..7])
+# create_data(index, type)
+
+result = @client.search_tag("game")
+#result = @client.search("")
 puts "------"
 p JSON.parse(result.body)
